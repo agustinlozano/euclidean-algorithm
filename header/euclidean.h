78586b4edbbc0ca381738c369282168b239be636
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2021-09-27 09:19:46 -0300

    Add comments to the header

--- header/euclidean.h
+++ header/euclidean.h
@@ -8,10 +8,23 @@ typedef struct {
     int numerator, denominator;
 } fraction;
 
-int mcd(fraction *f);
-
 fraction reduce(fraction *f);
+/*  
+ *  Es la funcion que se encarga de aplicar el algoritmo
+ *  a la fraccion original, depende de la funcion mcd para
+ *  calcular el maximo comun divisor, luego retorna la 
+ *  fraccion resultante.
+ */
+
+int mcd(fraction *f);
+/*  
+ *  Calcula el maximo comun divisor estructura tipo fraction,
+ *  luego lo retorna.
+ */
 
 void muestra(fraction f);
+/*  
+ *  Imprime el numerador y denominador de una estructura tipo fraction.  
+ */
 
 #endif
